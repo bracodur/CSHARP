315fb9c35b9bca2b726be6c45d17ce2ed4ec95e9
Jane Turova <turova@mail.ru>
2024-01-29 15:33:57 +0700

    add variable creation

--- LESSON1/Task1/Program.cs
+++ LESSON1/Task1/Program.cs
@@ -1,2 +1,15 @@
-﻿Console.WriteLine ("Hello world");
+﻿//Console.WriteLine ("Hello world");
+
+using System.ComponentModel;
+
+int value = 10;
+Console.WriteLine (value);
+
+double d = 5.55;
+char c = '!';
+bool b = true;
+Console.WriteLine (d);
+Console.WriteLine (c);
+Console.WriteLine (b);
+
 
