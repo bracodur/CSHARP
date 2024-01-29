5c1459ab374196acd9c9587bd2c7c7114293cd42
Jane Turova <turova@mail.ru>
2024-01-29 17:34:33 +0700

    add task4

---
+++ LESSON1/task4/Program.cs
@@ -0,0 +1,28 @@
+﻿int a = 5;
+int b = 6;
+int c = 6;
+int d = 6;
+int e = 6;
+int max = a;
+
+if(b>max)
+{
+    max = b;
+}
+if (c>max)
+{
+    max = c;
+}
+if (d>max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+
+Console.Write("Maximum is:");
+Console.WriteLine(max);
+
+    
