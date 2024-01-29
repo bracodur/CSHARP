e79ad1b8533f5173b754b9a70a38f59bf26a8996
Jane Turova <turova@mail.ru>
2024-01-29 16:16:24 +0700

    add task2

---
+++ LESSON1/task2/Program.cs
@@ -0,0 +1,30 @@
+﻿int i1 = 5;
+int i2 = 6;
+int sum = i1 + i2;
+Console.WriteLine(sum);
+ 
+ //сумма разных типов данных
+
+long l1 = 6;
+long sum2 = i1 + l1;
+
+double d1 = 5.5;
+double sum3 = i1 + d1;
+
+Console.WriteLine (sum2);
+Console.WriteLine (sum3);
+
+// произведение разных типов данных
+
+int product1 = i1 * i2;
+double product2 = i1 * d1;
+Console.WriteLine (product1);
+Console.WriteLine (product2);
+
+// деление разных типов данных
+//при операции деления может получиться не целое число,
+//поэтому лучше сразу использовать тип данных double
+
+double d2 = 6;
+double quotient2 = i1/d2;
+Console.WriteLine (quotient2);
