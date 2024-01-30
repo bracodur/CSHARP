a38b1ea83b68a252b6a6ad4b3d607718aff4e1e4
Jane Turova <turova@mail.ru>
2024-01-30 21:52:24 +0700

    add task1

---
+++ HomeWork/dz1/Program.cs
@@ -0,0 +1,21 @@
+﻿//Найти большее из двух чисел
+int firstNumber = 7;
+int secondNumber = 15;
+
+ if (firstNumber == secondNumber)
+{
+  Console.Write("Числа равны");
+}
+      else 
+{
+   if (firstNumber > secondNumber)
+    {
+        Console.Write("Первое число больше: ");
+        Console.WriteLine(firstNumber);
+     }
+     else 
+    {
+        Console.Write("Второе число больше: ");
+        Console.WriteLine(secondNumber);
+    }
+}
