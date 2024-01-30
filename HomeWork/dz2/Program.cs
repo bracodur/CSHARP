97c93e8b2734b050ed25d2c7aee21b5b3dafb3b0
Jane Turova <turova@mail.ru>
2024-01-30 22:06:17 +0700

    add dz2

---
+++ HomeWork/dz2/Program.cs
@@ -0,0 +1,19 @@
+﻿//найти максимальное из трех чисел
+int a = 9;
+int b = 8;
+int c = 7;
+
+int max = a;
+{
+ Console.WriteLine(a);
+}
+
+if (b > a)
+{
+ Console.WriteLine(b);
+}
+if (c>b)
+{
+ Console.WriteLine(c);
+}
+
