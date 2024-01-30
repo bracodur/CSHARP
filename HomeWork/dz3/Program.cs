6c82f82ba1e4297f0d7a47625fe7e42a207c2db0
Jane Turova <turova@mail.ru>
2024-01-30 22:48:28 +0700

    add dz3

---
+++ HomeWork/dz3/Program.cs
@@ -0,0 +1,11 @@
+﻿// определить четное или нечетное число
+int number = 3;
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("четное");
+}
+if (number % 2 != 0)
+{
+    Console.WriteLine("нечетное");
+}
