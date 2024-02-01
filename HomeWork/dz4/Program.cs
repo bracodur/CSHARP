ca32661e188ea230add07b0546d870fc472a463b
Jane Turova <turova@mail.ru>
2024-02-01 10:24:55 +0700

    add dz4

---
+++ HomeWork/dz4/Program.cs
@@ -0,0 +1,27 @@
+﻿// вывести все четные числа массива
+
+//int N = 8;
+//int array[] = { 2,3,4,5,6,7,8,9 };
+//int i = 0;
+
+//while (i < N)
+//{
+    //if (array[i] % 2 == 0)
+    //{
+       // Console.Write($"{array[i]} ");
+    //}
+    //i = i + 1;
+//}
+int number = 8;
+
+for (int i = 1; i< number; i++)
+
+    while (i <= number)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write($"{i} ");
+    }
+    i = i + 1;
+}
+
