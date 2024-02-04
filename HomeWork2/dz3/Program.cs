9edb7b068651b1639f1040bd1adeb2e965e16130
Jane Turova <turova@mail.ru>
2024-02-04 20:58:40 +0700

    add HomeWork\dz3

---
+++ HomeWork2/dz3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход целое число
+// из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+
+// /= разделение чисел(отсечение)
+
+int number = 32;
+int firstnumber = number / 10;
+int secondnumber = number % 10;
+//string str number = number.ToString;
+//str numbe
+//int maxnumber =
+if (firstnumber > secondnumber)
+{
+    System.Console.WriteLine (firstnumber);
+}
+else
+{
+    System.Console.WriteLine (secondnumber);
+}
+
