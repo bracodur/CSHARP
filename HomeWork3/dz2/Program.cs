f2d252132aeed7c8cb652684549951a7edfaf2ca
Jane Turova <turova@mail.ru>
2024-02-11 19:04:08 +0700

    add dz2

--- HomeWork3/dz2/Program.cs
+++ HomeWork3/dz2/Program.cs
@@ -1,15 +1,26 @@
-﻿Random rnd = new Random();
+﻿//Задайте массив заполненный случайными трехзначными числами.
+//написать программу которая покажет количество четных чисел в массиве.
+
+Random rnd = new Random();
+
 int size = 1000;
 int[] arr_int = new int[size];
 int i = 0;
+int KolichestvoEven = 0;
 while (i < size)
 {
-    arr_int [i] = rnd.Next (99, 1000);
+    arr_int[i] = rnd.Next(99, 1000);
     i = i + 1;
 }
+
 i = 0;
 while (i < size)
 {
-    Console.Write($"{arr_int[i]} ");
+    //Console.Write($"{arr_int[i]} ");
+
+    if (arr_int[i] % 2 == 0)
+        KolichestvoEven++;
+
     i = i + 1;
 }
+Console.Write($"Количество четных чисел:{KolichestvoEven} ");
