ddaab894874df5f1dca5b497cc9430eaa7648e6a
Jane Turova <turova@mail.ru>
2024-02-11 12:37:49 +0700

    add dz2

---
+++ HomeWork3/dz2/Program.cs
@@ -0,0 +1,15 @@
+﻿Random rnd = new Random();
+int size = 1000;
+int[] arr_int = new int[size];
+int i = 0;
+while (i < size)
+{
+    arr_int [i] = rnd.Next (99, 1000);
+    i = i + 1;
+}
+i = 0;
+while (i < size)
+{
+    Console.Write($"{arr_int[i]} ");
+    i = i + 1;
+}
