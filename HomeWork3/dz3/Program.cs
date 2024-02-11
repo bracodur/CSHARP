232e7d3c21582189f5f731f2d8f0df21a61f4697
Jane Turova <turova@mail.ru>
2024-02-11 19:54:13 +0700

    add dz3

---
+++ HomeWork3/dz3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Написать программу, которая перевернет одномерный массив. 
+//Первый элемент станет последним, второй - предпоследним и т.д
+
+int[] numbers = {1, 3, 5, 6, 7, 8}; 
+int temp;
+
+for (int i = 0; i < numbers.Length / 2; i++)
+{
+temp = numbers[i];
+numbers[i] = numbers[numbers.Length - 1 - i];
+numbers[numbers.Length - 1 - i] = temp;
+}
+foreach (int number in numbers)
+{
+Console.Write(number + " ");
+}
+
