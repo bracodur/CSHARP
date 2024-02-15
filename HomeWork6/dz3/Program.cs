8311b7fac193f6fcb320c701f3c5ae1f957a3aa9
Jane Turova <turova@mail.ru>
2024-02-15 09:32:22 +0700

    add dz3

---
+++ HomeWork6/dz3/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задайте произвольную строку. Выясните, является ли она палиндромом.
+
+//char[] array = new char[];
+//char[] palindrom = new char[];
+//char temp;
+string array;
+string palindrom="yes";
+
+array = Console.ReadLine();
+ 
+for (int i = 0; i < array.Length/2; i++)
+{
+    if( array[i] != array[array.Length - 1 - i])
+        palindrom="no";
+}
+
+Console.Write(palindrom);
