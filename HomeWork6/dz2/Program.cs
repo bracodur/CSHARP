46c4adc1a5032badf1a062727b37ee53132d78d0
Jane Turova <turova@mail.ru>
2024-02-15 09:33:38 +0700

    add dz2

---
+++ HomeWork6/dz2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задайте строку, содержащую латинские буквы в обоих регистрах.
+//Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+//“aBcD1ef!-” => “abcd1ef!-”
+
+//char[] ch_array = { 'a', 'B', 'c', 'D','l', 'e', 'f', '!', '-' };
+
+
+//Console.Write("Введите символы: ");
+
+//char[] ch_array = Console.ReadLine(); 
+Console.WriteLine("Input High and Low Case Lat Symbols");
+string input = Console.ReadLine(); 
+Console.WriteLine("Input:");
+Console.WriteLine(input);
+Console.WriteLine("To Lower:");
+Console.WriteLine(input.ToLower());
